fbc24d3b692413e1ab90a3c78398796188af8f4a
Doxstart <127298109+Doxstart@users.noreply.github.com>
2023-04-28 13:15:22 +0200

    Pokedex version 2
    lezione 28/04/2023

---
+++ app.js
@@ -0,0 +1,58 @@
+console.log('eccolo!!!')
+
+let actualPageIndex
+
+getNextPage();
+
+function getNextPage(){
+    if (actualPageIndex === undefined) {
+        actualPageIndex = 0;
+    } else {
+        actualPageIndex++;
+    }
+    PokeService.getPage(actualPageIndex).then(pokemons => {
+        displayPokemons(pokemons);
+    })
+}
+
+function getPreviousPage(){
+    actualPageIndex--;
+
+    PokeService.getPage(actualPageIndex).then(pokemons => {
+        displayPokemons(pokemons);
+    })
+}
+
+function displayPokemons(pokemons){
+    const pokemonContainer = document.getElementById('pokemon-container');
+    pokemonContainer.innerHTML = '';
+    for (const pokemon of pokemons) {
+        
+        
+
+        pokemonContainer.innerHTML += `
+        <details>
+            <summary>
+                <span>${pokemon.id}</span>
+                <img class="list-img" src="${pokemon.sprites.front_default}" alt="">
+                <span>${pokemon.name}</span>
+                <div class="spacer"></div>
+                ${pokemon.types.map(obj => `<span class="type">${obj.type.name}</span>`).join('')}
+            </summary>
+            <div>
+                <ul>
+                ${createAbilitiesList(pokemon)}
+                </ul>
+            </div>
+        </details>
+        `
+    }
+}
+
+function createAbilitiesList(pokemon){
+    let abilitiesHtml = '';
+    for (const object of pokemon.abilities) {
+        abilitiesHtml += `<li>${object.ability.name}</li>`   
+    }
+    return abilitiesHtml;
+}
